397ae5774e2fe7b4aecbb8febd2459f2441fcfa1
MariaLuisaGabriel <maria.luisa.gabriel.domingues@ufu.br>
2024-01-16 11:58:44 -0300

    resolução problema C otimizado

--- 13.01/C/Resolucao-C-otimizado.cpp
+++ 13.01/C/Resolucao-C-otimizado.cpp
@@ -1,26 +1,25 @@
-//matriz 100*100 = 0.202 segundos
+//matriz 100*100 = 0.126 segundos
 
 #include <iostream>
 #include <queue>
 #include <chrono>
 
-int op1(int n,int **lib){
+int op1(int n,int *lib){
     int friends[4] = {0,0,0,0};
     int Max=0,f;
+    int aux1 = n/2, aux2 = n*n/2;//economizar custo com aritmética
 
     //cada amigo, na ordem "JOANA, PEDRO, ARTHUR, SABRINA", recebe o somatório 
     //da qualidade dos livros que os pertencem
-    for(int line=0; line<n; line++){
-        for(int col=0; col<n; col++){
-            if(line<n/2){
-                if(col<n/2) friends[0]+=lib[line][col];
-                else friends[1]+=lib[line][col];
-            }
-            else{
-                if(col<n/2) friends[2]+=lib[line][col];
-                else friends[3]+=lib[line][col];
-            }
+    for(int i = 0; i<aux2;){
+        if(i<n/2){
+            friends[0]+=lib[i];
+            friends[1]+=lib[i+aux1];
+            friends[2]+=lib[i+aux2];
+            friends[3]+=lib[i+aux2+aux1];
+            i++;
         }
+        else i+=aux1;
     }
 
     //decisão de qual é o amigo que possui os melhores livros
@@ -30,7 +29,7 @@ int op1(int n,int **lib){
             f=i;
         }
         else if(friends[i]==Max) {
-            return -1;
+            f=-1;
         }
     }
 
@@ -46,12 +45,18 @@ int main(){
 
     //Recebendo os dados de entrada:
     std::cin >> N;
-    int **lib = new int*[N];
+    if(N%2==1) return 0;
 
-    for(int line = 0; line<N; line++){
-        lib[line] = new int[N]; 
-        for(int col = 0; col<N; col++){
-            std::cin >> lib[line][col];
+    int *lib = new int[N*N];
+
+    if(N%4 == 0){
+        for(int i = 0; i<N*N; i+=4){ 
+            std::cin >> lib[i] >> lib[i+1] >> lib[i+2] >> lib[i+3];
+        }
+    }
+    else{
+        for(int i = 0; i<N*N; i+=2){ 
+            std::cin >> lib[i] >> lib[i+1];
         }
     }
 
@@ -62,7 +67,7 @@ int main(){
         if(op==1) q.push(op1(N,lib));
         if(op==2) {
             std::cin >> i >> j >> x;
-            lib[i-1][j-1] = x;
+            lib[i*N+j] = x;
         }
     }
 
@@ -87,9 +92,7 @@ int main(){
             break;
         }
     }
-
     end = clock();
-    printf("\ntempo de execução: %0.20f",((double)end-start)/CLOCKS_PER_SEC);
-
+    printf("\ntempo de execução: %0.8f",((float)end-start)/CLOCKS_PER_SEC);
     return 0;
 }
