611db1c19d1b215f0cf117ef9631e5364a269ebd
MariaLuisaGabriel <maria.luisa.gabriel.domingues@ufu.br>
2024-03-11 18:25:49 -0300

    resolvido

--- setters/aritmeticaBusu.cpp
+++ setters/aritmeticaBusu.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <vector>
+#include <queue>
 #include <algorithm>
 #include <string>
 #include <cstdio>
@@ -7,10 +7,41 @@
 using namespace std;
 
 int mdc(int a, int b){
-    if(a==0) return b;
+    if(b==0) return a;
     mdc(b,a%b);
 }
 
-int mmc(){
+int mmc(int a, int b){
+    return a*b/mdc(a,b);
+}
 
+int main(){
+    int n,m;
+    queue<int> horas,horas1;
+    cin >> n;
+    for(int i = 0; i<n; i++){
+        cin >> m;
+        horas.push(m);
+        horas1.push(m);
+    }
+
+    while(horas.size()!=1){
+        n = horas.front();
+        horas.pop();
+        m = horas.front();
+        horas.pop();
+
+        horas.push(mmc(n,m));
+    }
+
+    n = horas.front();
+    int i = 1;
+
+    while(!horas1.empty()){
+        cout << "onibus " << i << " : " << n/horas1.front() << " voltas;" << endl;
+        horas1.pop();
+        i++;
+    }
+
+    return 0;
 }
